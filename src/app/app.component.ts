fa52888088b3dfc071c9fdb6b31980395e2658eb
tw1ttt3r <tw1ttt3r@gmail.com>
2023-12-04 09:00:17 -0600

    uso de componentes y directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import menu from "../assets/menu"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mini-instagram';
+  title = 'Bienvenidos a mi perfil';
+  menu = menu;
+
+  ngOnInit() {
+    console.log("Hola desde mi componente, esto solo se vera un vez");
+  }
+
+  ngAfterViewInit() {
+    console.log("Hola desde mi componente, esto solo se vera un vez después de que se inicializo mi vista");
+  }
+
+  ngAfterViewChecked() {
+    console.log("Hola desde mi componente, esto solo se vera un vez después de que hay cambios en mi vista");
+  }
+  
+  ngOnDestroy() {
+    console.log("Se verá hasta que mi componente se vaya o se destruya");
+  }
 }
---
+++ src/assets/menu.ts
@@ -0,0 +1,45 @@
+import { MenuOption } from "../types/MenuOption.type"
+
+const menu: Array<MenuOption> = [
+    {
+        icono: "",
+        titulo: "Inicio",
+    },
+    {
+        icono: "",
+        titulo: "Buscar",
+        ruta: ""
+    },
+    {
+        icono: "",
+        titulo: "Explorar",
+        ruta: ""
+    },
+    {
+        icono: "",
+        titulo: "Reels",
+        ruta: ""
+    },
+    {
+        icono: "",
+        titulo: "Mensajes",
+        ruta: ""
+    },
+    {
+        icono: "",
+        titulo: "Notificaciones",
+        ruta: ""
+    },
+    {
+        icono: "",
+        titulo: "Crear",
+        ruta: ""
+    },
+    {
+        icono: "",
+        titulo: "Perfil",
+        ruta: ""
+    },
+];
+
+export default menu
