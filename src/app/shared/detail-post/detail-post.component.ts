e1f2387414574a170844ecbe19e30deaebc00d80
tw1ttt3r <tw1ttt3r@gmail.com>
2023-12-07 09:15:50 -0600

    se agrega ejemplo para consumo de ruta dinamica en el componente

--- src/app/shared/detail-post/detail-post.component.ts
+++ src/app/shared/detail-post/detail-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-post',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-post.component.scss']
 })
 export class DetailPostComponent {
+
+  // De esta manera podemos obtener el valor de la url como parametro dinamico
+  constructor(private router: ActivatedRoute) {
+    console.log("router", this.router.snapshot.params)
+  }
 
 }
